3b89edeadba65b51791e29a891074db8fd7030f1
s-iwsk1989 <47kr.miyosuke@gmail.com>
2024-09-13 16:33:42 +0900

    create project

---
+++ src/main/java/com/plant34diary/caring_diary_api/CaringDiaryApiApplication.java
@@ -0,0 +1,13 @@
+package com.plant34diary.caring_diary_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaringDiaryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaringDiaryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plant34diary/caring_diary_api/CaringDiaryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plant34diary.caring_diary_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaringDiaryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
